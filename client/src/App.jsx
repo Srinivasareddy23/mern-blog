a1a430d4bbcef18038368b5ad7f9101e70634b94
Srinivasareddy23 <srinivasa1029@gmail.com>
2024-06-11 11:30:30 +0530

    Added Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div>
-      <h1 className='bg-red-500 p-20 text-xl text-white font-sans text-center'>This is Mern stack Blog Project.</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="signin" element={<Signin />}/>
+        <Route path="signup" element={<Signup />}/>
+        <Route path="projects" element={<Projects />}/>
+        <Route path="about" element={<About />}/>
+        <Route path="dashboard" element={<Dashboard />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
