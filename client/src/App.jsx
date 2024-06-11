594de09381cd13e53e0d2d11bc8dbd1d1894f164
Srinivasareddy23 <srinivasa1029@gmail.com>
2024-06-11 10:17:57 +0530

    React setup and tailwind css setup has been done , and working with mern stack

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-red-500 p-20 text-xl text-white font-sans text-center'>This is Mern stack Blog Project.</h1>
+    </div>
+  )
+}
+
+export default App
